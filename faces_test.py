e6fb2715123d5926602865df733c1f6904f9cdf5
Mel <melodylu@gmail.com>
2017-08-17 16:13:08 -0700

    fix test to look for unnatural_face_yellow

--- faces_test.py
+++ faces_test.py
@@ -27,13 +27,13 @@ def test_main(tmpdir):
     # Make sure there isn't already a green box
     im = Image.open(in_file)
     pixels = im.getdata()
-    greens = sum(1 for (r, g, b) in pixels if r == 0 and g == 255 and b == 0)
-    assert greens < 1
+    unnatural_face_yellow = sum(1 for (r, g, b) in pixels if r == 251 and g == 200 and b == 83)
+    assert unnatural_face_yellow < 1
 
     main(in_file, out_file, 10)
 
     # Make sure there now is some green drawn
     im = Image.open(out_file)
     pixels = im.getdata()
-    greens = sum(1 for (r, g, b) in pixels if r == 0 and g == 255 and b == 0)
-    assert greens > 10
+    unnatural_face_yellow = sum(1 for (r, g, b) in pixels if r == 251 and g == 200 and b == 83)
+    assert unnatural_face_yellow > 10
