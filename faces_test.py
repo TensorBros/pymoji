e4b2b7c5dc66cbede1e1dcf011ff4ef722ba0d3f
Mel <melodylu@gmail.com>
2017-08-17 16:39:12 -0700

    PR review fixes for clarity

--- faces_test.py
+++ faces_test.py
@@ -30,7 +30,7 @@ def test_main(tmpdir):
     unnatural_face_yellow = sum(1 for (r, g, b) in pixels if r == 251 and g == 200 and b == 83)
     assert unnatural_face_yellow < 1
 
-    main(in_file, out_file, 10)
+    main(in_file, out_file)
 
     # Make sure there now is some green drawn
     im = Image.open(out_file)
