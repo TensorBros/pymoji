5ec59747fc22272c8004c19b6a86d123aa250791
Benjamin Lu <lu.s.benjamin@gmail.com>
2017-08-21 18:28:14 -0700

    refactor faces

--- manage.py
+++ manage.py
@@ -1,44 +1,34 @@
 """Script manager for running locally. Gunicorn is used to run the
 application on Google App Engine. See entrypoint in app.yaml.
 """
-import os
-
 from flask_script import Manager
 
-from pymoji import faces, utils
 from pymoji.app import APP
-from pymoji.constants import OUTPUT_DIR, STATIC_DIR
+from pymoji.faces import process_path
+from pymoji.utils import process_folder
 
 
 MANAGER = Manager(APP)
 
 
 @MANAGER.command
-def runface(input_image=None, output_image=None):
-    """Processes faces in the specified image.
+def runface(image_path):
+    """Processes faces in the given image.
 
     Args:
-        input_image: name of image resource file to process faces in.
-        output_image: name of output file to write modified image to. (optional)
+        image_path: path to an image file to process faces in.
     """
-    input_path = os.path.join(STATIC_DIR, input_image)
-
-    if output_image:
-        output_path = os.path.join(OUTPUT_DIR, output_image)
-    else:
-        output_path = utils.generate_output_path(input_image)
-
-    faces.main(input_path, output_path)
+    process_path(image_path)
 
 
 @MANAGER.command
-def runfolder(directory=None):
-    """Processes images in the specified directory.
+def rundir(directory_path):
+    """Processes images in the given directory.
 
     Args:
-        directory: path to directory to process images in.
+        directory_path: path to a directory to process images in.
     """
-    utils.process_folder(directory, faces.main)
+    process_folder(directory_path, process_path)
 
 
 if __name__ == "__main__":
--- tests/faces_test.py
+++ tests/faces_test.py
@@ -4,12 +4,12 @@ import os
 from PIL import Image
 
 from pymoji.constants import STATIC_DIR
-from pymoji.faces import main
+from pymoji.faces import process_path
 from pymoji.utils import generate_output_path
 
 
-def test_main():
-    """tests pymoji.faces.main"""
+def test_process_path():
+    """tests pymoji.faces.process_path"""
     out_file = generate_output_path('face-input.jpg')
     in_file = os.path.join(STATIC_DIR, 'face-input.jpg')
 
@@ -19,7 +19,7 @@ def test_main():
     unnatural_face_yellow = sum(1 for (r, g, b) in pixels if r == 251 and g == 200 and b == 83)
     assert unnatural_face_yellow < 1
 
-    main(in_file, out_file)
+    process_path(in_file)
 
     # Make sure there now is some emoji-yellow drawn
     image = Image.open(out_file)
