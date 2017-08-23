529a443a6fac0516556b40061161dfc16b30f14e
Benjamin Lu <lu.s.benjamin@gmail.com>
2017-08-23 12:25:40 -0700

    hook up DEMO_PATH to default emojivision route

--- tests/faces_test.py
+++ tests/faces_test.py
@@ -3,22 +3,21 @@ import os
 
 from PIL import Image
 
-from pymoji.constants import OUTPUT_DIR, UPLOADS_DIR
+from pymoji.constants import DEMO_PATH, OUTPUT_DIR
 from pymoji.faces import process_path
 from pymoji.utils import get_output_name
 
 
 def test_process_path():
     """tests pymoji.faces.process_path"""
-    in_path = os.path.join(UPLOADS_DIR, 'face-input.jpg')
 
     # Make sure there isn't already any emoji-yellow
-    image = Image.open(in_path)
+    image = Image.open(DEMO_PATH)
     pixels = image.getdata()
     unnatural_face_yellow = sum(1 for (r, g, b) in pixels if r == 251 and g == 200 and b == 83)
     assert unnatural_face_yellow < 1
 
-    id_filename = process_path(in_path)
+    id_filename = process_path(DEMO_PATH)
     output_filename = get_output_name(id_filename)
     out_path = os.path.join(OUTPUT_DIR, output_filename)
 
