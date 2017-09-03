61f5e19c781543934ce3c17ac11b5047222fe655
Benjamin Lu <lu.s.benjamin@gmail.com>
2017-09-02 22:21:39 -0700

    rank-based emoji engine
    - collect N candidate emoji codes
    - use funniest based on humor rank
    - process faces by depth rank order

--- pymoji/emoji.py
+++ pymoji/emoji.py
@@ -10,7 +10,7 @@ from PIL import Image, ImageDraw
 
 from pymoji import FACE_PAD, USE_BIG_GUNS
 from pymoji.constants import EMOJI_CDN_PATH
-from pymoji.constants import VERY_UNLIKELY, UNLIKELY, POSSIBLE, LIKELY, VERY_LIKELY
+from pymoji.constants import UNLIKELY, POSSIBLE, LIKELY, VERY_LIKELY
 from pymoji.utils import download_image
 from pymoji.vision import detect_labels, to_vision_image
 
@@ -21,66 +21,117 @@ from pymoji.vision import detect_labels, to_vision_image
 EMOJI = {} # cache
 
 DEFAULT_CODE = "1f642" # slightly smiling face
-SORROW_CODES = [
-    "1f641", # slightly frowning face
+
+SORROW_MAP = {
+    UNLIKELY: "1f641", # slightly frowning face
     #"2639", # frowning face
-    "1f61f", # worried face
-    "1f61e", # disappointed face
-    "1f622", # crying face
+    POSSIBLE: "1f61f", # worried face
+    LIKELY: "1f61e", # disappointed face
+    VERY_LIKELY: "1f622", # crying face
     # "1f62d", # loudly crying face
-]
-ANGER_CODES = [
-    "1f610", # neutral face
-    "1f610", # neutral face
-    "1f620", # angry face
-    "1f620", # angry face
+}
+
+ANGER_MAP = {
+    UNLIKELY: "1f610", # neutral face
+    POSSIBLE: "1f610", # neutral face
+    LIKELY: "1f620", # angry face
+    VERY_LIKELY: "1f620", # angry face
     # "1f621", # pouting face
     # "1f624", # face with steam from nose
     # "1f92c", # face with symbols over mouth
-]
-SURPRISE_CODES = [
-    "1f62f", # hushed face
-    "1f62e", # face with open mouth
+}
+
+SURPRISE_MAP = {
+    UNLIKELY: "1f62f", # hushed face
+    POSSIBLE: "1f62e", # face with open mouth
     # "1f627", # anguished face
-    "1f632", # astonished face
-    "1f628", # fearful face
+    LIKELY: "1f632", # astonished face
+    VERY_LIKELY: "1f628", # fearful face
     # "1f631", # face screaming in fear
-]
-JOY_CODES = [
+}
+
+JOY_MAP = {
     # "1f642", # slightly smiling face
-    "1f60c", # relieved face
+    UNLIKELY: "1f60c", # relieved face
     # "263a", # smiling face
     # "1f60a", # smiling face with smiling eyes & rosy cheeks
     # "1f600", # grinning face
-    "1f603", # smiling face with open mouth
-    "1f601", # grinning face with smiling eyes
+    POSSIBLE: "1f603", # smiling face with open mouth
+    LIKELY: "1f601", # grinning face with smiling eyes
     # "1f604", # smiling face with open mouth & smiling eyes
-    "1f606", # smiling face with open mouth & closed eyes
+    VERY_LIKELY: "1f606", # smiling face with open mouth & closed eyes
     # "1f602", # face with tears of joy
     # "1f923", # rolling on the floor laughing
+}
+
+# earlier in list ~ funnier / rarer
+HUMOR_RANK = [
+    # sorrow likelihood
+    "1f622", # crying face
+    "1f61e", # disappointed face
+    "1f61f", # worried face
+    "1f641", # slightly frowning face
+
+    # anger likelihood
+    "1f620", # angry face
+    "1f610", # neutral face
+
+    # surprise likelihood
+    "1f628", # fearful face
+    "1f632", # astonished face
+    "1f62e", # face with open mouth
+    "1f62f", # hushed face
+
+    # joy likelihood - rare
+    "1f606", # smiling face with open mouth & closed eyes
+    "1f601", # grinning face with smiling eyes
+
+    # headwear likelihood
+    "1f920", # cowboy hat face
+
+    # big guns
+    "1f60e", # smiling face with sunglasses
+    "1f913", # nerd face
+
+    # joy likelihood - boring
+    "1f603", # smiling face with open mouth
+    "1f60c", # relieved face
+
+    # default
+    "1f642", # slightly smiling face
+
+    # currently impossible, but someday...
+    "1f615", # confused face
+    "1f61b", # face with stuck-out tongue
+    "1f60d", # smiling face with heart-eyes
+    "1f644", # face with rolling eyes
 ]
-# MISC CODES
-# "1f615" # confused face
-# "1f913" # nerd face
-# "1f60e" # smiling face with sunglasses
-# "1f61b" # face with stuck-out tongue
-# "1f60d" # smiling face with heart-eyes
-# "1f644" # face with rolling eyes
 
 
-def compute_emoji_box(image, face, face_pad=FACE_PAD):
-    """Computes a 4-tuple defining the left, upper, right, and lower pixel
-    coordinate for the emoji bounding box based on the given image and face
-    annotation metadata. Remember that the upper-left corner is the origin!
+def get_humor_rank(code):
+    """Computes a humor ranking value for the given emoji code. A lower rank
+    roughly means a funnier or rarer emoji.
 
     Args:
-        image: a PIL.Image
-        face: a face annotation object from the Google Vision API.
-        face_pad: percentage to enlarge emoji beyond face bounding box
+        code: a string containing an emoji code
 
     Returns:
-        a 4-tuple defining the left, upper, right, and lower pixel coordinate
-            e.g. (0, 0, 128, 128)
+        an integer humor rank, lower is better
+    """
+    return HUMOR_RANK.index(code)
+
+
+def get_face_box(face):
+    """Returns a 4-tuple defining the left, upper, right, and lower pixel
+    coordinate for the bounding box of the given face annotation metadata.
+    Remember that the upper-left corner is the origin!
+
+    Args:
+        face: a face annotation object from the Google Vision API.
+
+    Returns:
+        a 4-tuple defining the left, upper, right, and lower pixel of the face
+        bounding-box
     """
     face_top_left = face.bounding_poly.vertices[0]
     face_left = face_top_left.x
@@ -90,6 +141,43 @@ def compute_emoji_box(image, face, face_pad=FACE_PAD):
     face_right = face_bottom_right.x
     face_bottom = face_bottom_right.y
 
+    return (face_left, face_top, face_right, face_bottom)
+
+
+def get_depth_rank(face):
+    """Computes a ranking value for the given face annotation metadata object.
+    Initially based on a super crude approximation of depth.
+
+    Depth approximation: humans are highly unlikely to be hanging-upside down,
+        therefore a taller face is further away, break ties with area
+
+    Note: initial tests using area as a proxy for depth yielded unstable results.
+
+    Args:
+        face: a face annotation object from the Google Vision API.
+
+    Returns:
+        a float value roughly approximating depth
+    """
+    (_face_left, face_top, _face_right, _face_bottom) = get_face_box(face)
+    return face_top
+
+
+def get_emoji_box(face, face_pad=FACE_PAD, code=None):
+    """Computes a 4-tuple defining the left, upper, right, and lower pixel
+    coordinate for the emoji bounding box based on the given face
+    annotation metadata. Remember that the upper-left corner is the origin!
+
+    Args:
+        face: a face annotation object from the Google Vision API.
+        face_pad: percentage to enlarge emoji beyond face bounding box
+
+    Returns:
+        a 4-tuple defining the left, upper, right, and lower pixel coordinate
+            e.g. (0, 0, 128, 128)
+    """
+    (face_left, face_top, face_right, face_bottom) = get_face_box(face)
+
     # compute height and width (top-left corner is origin)
     face_height = face_bottom - face_top
     face_width = face_right - face_left
@@ -98,14 +186,15 @@ def compute_emoji_box(image, face, face_pad=FACE_PAD):
     width_pad = face_width * face_pad
     height_pad = face_height * face_pad
 
-    # original image bounding box
-    (image_left, image_top, image_right, image_bottom) = image.getbbox()
+    # add padding
+    left = int(face_left - width_pad)
+    top = int(face_top - height_pad)
+    right = int(face_right + width_pad)
+    bottom = int(face_bottom + height_pad)
 
-    # add padding, use max and min to limit based on outer image
-    left = max(image_left, int(face_left - width_pad))
-    top = max(image_top, int(face_top - height_pad))
-    right = min(image_right, int(face_right + width_pad))
-    bottom = min(image_bottom, int(face_bottom + height_pad))
+    if code == "1f920": # cowboy hat face
+        # TODO cherry-pick fancy scaling here
+        pass
 
     return (left, top, right, bottom)
 
@@ -117,7 +206,7 @@ def get_emoji_image(code, box):
 
     Args:
         code: a string containing the code for the desired emoji.
-        box: a 4-tuple defining the emoji bounding box (see compute_emoji_box)
+        box: a 4-tuple defining the emoji bounding box (see get_emoji_box)
 
     Returns:
         a scaled RGBA PIL.Image of the emoji.
@@ -137,60 +226,34 @@ def get_emoji_image(code, box):
     return EMOJI[code].resize((width, height), resample=0)
 
 
-def get_code(likelihood, code_list):
-    """Returns a code from the given list based on the given likelihood. Super basic.
-
-    https://cloud.google.com/vision/docs/reference/rest/v1/images/annotate#likelihood
-
-    Args:
-        likelihood: an integer representing google vision likelihood
-        code_list: a list containing exactly 4 emoji codes in ascending order
-
-    Returns:
-        an emoji string code
-    """
-    if likelihood == VERY_LIKELY:
-        return code_list[3]
-    elif likelihood == LIKELY:
-        return code_list[2]
-    elif likelihood == POSSIBLE:
-        return code_list[1]
-    elif likelihood == UNLIKELY:
-        return code_list[0]
-    return DEFAULT_CODE
-
-
-def compute_emoji_code(image, face, box, use_big_guns=USE_BIG_GUNS):
+def get_emoji_code(image, face, use_big_guns=USE_BIG_GUNS):
     """Computes the 'best' emoji string code for the given face in the given
     image.
 
     Args:
-        image: a PIL.Image
+        image: the original PIL.Image containing the face
         face: a face annotation object from the Google Vision API.
-        box: a 4-tuple defining the emoji bounding box (see compute_emoji_box)
         use_big_guns: whether or not to fallback on label analysis (slow)
 
     Returns:
         an emoji string code
     """
+    sorrow_code = SORROW_MAP.get(face.sorrow_likelihood, DEFAULT_CODE)
+    anger_code = ANGER_MAP.get(face.anger_likelihood, DEFAULT_CODE)
+    surprise_code = SURPRISE_MAP.get(face.surprise_likelihood, DEFAULT_CODE)
+    joy_code = JOY_MAP.get(face.joy_likelihood, DEFAULT_CODE)
 
-    # check likelihood scores in roughly inverse-frequency order
-    # i.e. ensure that rare sorrow emoji outrank common joy emoji
-    if face.sorrow_likelihood > VERY_UNLIKELY:
-        return get_code(face.sorrow_likelihood, SORROW_CODES)
-    elif face.anger_likelihood > VERY_UNLIKELY:
-        return get_code(face.anger_likelihood, ANGER_CODES)
-    elif face.surprise_likelihood > VERY_UNLIKELY:
-        return get_code(face.surprise_likelihood, SURPRISE_CODES)
-    elif face.headwear_likelihood > POSSIBLE:
-        return "1f920" # cowboy hat face
-    elif face.joy_likelihood > VERY_UNLIKELY:
-        return get_code(face.joy_likelihood, JOY_CODES)
-    elif use_big_guns:
+    candidates = set([sorrow_code, anger_code, surprise_code, joy_code])
+
+    if face.headwear_likelihood > POSSIBLE:
+        candidates.add("1f920") # cowboy hat face
+
+    if len(candidates) == 1 and use_big_guns:
         # BRING OUT THE BIG GUNS - analyze labels on individual head
 
         # crop head + save into temp file
-        head_image = image.crop(box)
+        head_box = get_emoji_box(face)
+        head_image = image.crop(head_box)
         with TemporaryFile() as head_stream:
             head_image.save(head_stream, format='JPEG')
             head_stream.seek(0)
@@ -199,14 +262,18 @@ def compute_emoji_code(image, face, box, use_big_guns=USE_BIG_GUNS):
             gv_head_image = to_vision_image(input_stream=head_stream)
             labels = detect_labels(gv_head_image)
 
-            # greedily convert first interesting label into emoji
+            # greedily convert first interesting label into emoji, then stop
             for label in labels:
                 if label.description == "sunglasses": # at night so I can so I caaaaan
-                    return "1f60e" # smiling face with sunglasses
+                    candidates.add("1f60e") # smiling face with sunglasses
+                    break
                 elif label.description == "glasses":
-                    return "1f913" # nerd face
+                    candidates.add("1f913") # nerd face
+                    break
 
-    return DEFAULT_CODE
+    # sort by humor rank and return the funniest
+    ranked_candidates = sorted(candidates, key=get_humor_rank)
+    return ranked_candidates[0]
 
 
 def render_emoji(image, face):
@@ -219,10 +286,11 @@ def render_emoji(image, face):
         image: a PIL.Image
         face: a face annotation object from the Google Vision API.
     """
-    emoji_box = compute_emoji_box(image, face)
-    emoji_code = compute_emoji_code(image, face, emoji_box)
-    emoji = get_emoji_image(emoji_code, emoji_box)
-    image.paste(emoji, emoji_box, emoji)
+    code = get_emoji_code(image, face)
+    box = get_emoji_box(face, code=code)
+    emoji = get_emoji_image(code, box)
+    mask = emoji
+    image.paste(emoji, box, mask)
 
 
 def replace_faces(input_stream, faces, output_stream):
@@ -238,8 +306,11 @@ def replace_faces(input_stream, faces, output_stream):
         output_stream: a file-like-object to write the result to.
     """
     output_image = Image.open(input_stream)
-    for face in faces:
+    faces_by_depth = sorted(faces, key=get_depth_rank)
+
+    for face in faces_by_depth:
         render_emoji(output_image, face)
+
     output_image.save(output_stream)
     output_image.close()
 
