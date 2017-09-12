de75b50ecb0230e858453a14d4f6b06621b4d99c
Benjamin Lu <lu.s.benjamin@gmail.com>
2017-09-11 20:00:05 -0700

    convert some utils_test cases to doctests

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -6,13 +6,6 @@ from pymoji import utils
 from tests import TEST_JSON_PATH
 
 
-def test_allowed_file():
-    """tests pymoji.utils.allowed_file"""
-    assert utils.allowed_file('input-face.jpeg')
-    assert utils.allowed_file('__stoo-pid.file-name.PNG')
-    assert not utils.allowed_file('bar.mov')
-
-
 def test_save_to_cloud():
     """ tests pymoji.utils.save_to_cloud"""
     pass
@@ -66,18 +59,6 @@ def test_get_id_name():
     assert filename in name_two
     assert name_one != name_two
     assert name_two > name_one
-
-
-def test_get_json_name():
-    """ tests pymoji.utils.get_json_name"""
-    assert utils.get_json_name('input-face.jpg') == 'input-face-meta.json'
-    assert utils.get_json_name('__stoo-pid.file-name.PNG') == '__stoo-pid.file-name-meta.json'
-
-
-def test_get_output_name():
-    """ tests pymoji.utils.get_output_name"""
-    assert utils.get_output_name('input-face.jpg') == 'input-face-output.jpg'
-    assert utils.get_output_name('__stoo-pid.file-name.PNG') == '__stoo-pid.file-name-output.PNG'
 
 
 def test_process_folder():
